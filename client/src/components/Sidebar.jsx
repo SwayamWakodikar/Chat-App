3aea07c67d69cf277d918efc984ee9594d0b6ac1
SwayamWakodikar <swayam.w06@gmail.com>
2025-10-20 16:37:52 +0530

    Added user list

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -23,11 +23,25 @@ const Sidebar = ({selectedUser,setSelectedUser}) => {
           <input type="text" className='bg-transparent border-none outline-none text-white text-xs plaveholder-[#c8c8c8] flex-1' placeholder='Search User....' />
         </div>
       </div>
-
-      {/* Add user data in this */}
-      {/* <div className='flex flex-col'>
-        {userDummyData.map(userDummyData,index)}
-      </div> */}
+     {/* added user list */}
+      <div className='flex flex-col'>
+        {userDummyData.map((user,index)=>(
+          <div onClick={()=>{setSelectedUser(user)}} key={index} className={`relativ flex-items-center gap-2 p-2 pl-4 rounded cursor-pointer max-sm:text-sm ${selectedUser?._id===user._id&& 'bg-[#2821420]/50'}`
+ 
+          }>
+            <img src={user?.profilePic||assets.avatar_icon} alt="" className='w-[35px] asepect-[1/1] rounded-full'/>
+            <div className='flex flex-col leading-5'>
+              <p>{user.fullName}</p>
+              {
+                index<3?<span className='text-green-400 text-xs'>Online</span>:<span className='text-red-500 text-xs'>Offline</span>
+              }
+            </div>
+            {
+              index>2&& <p className='absolute top-4 right-4 text-xs h-5 w-5 flex justify-center rounded-full bg-violet-500/50'>{index}</p>
+            }
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -8,10 +8,10 @@ const Homepage = () => {
     const[selectedUser,setSelcetedUser]=useState(false);
   return (
     <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'>
-        <div className='backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%] grid grid-cols-1 relative'>
-            <Sidebar/>
-            <Chatcontainer/>
-            <RightSidebar/>
+        <div className='backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%] grid grid-cols-3 relative'>
+            <Sidebar selectedUser={selectedUser} setSelectedUser={setSelcetedUser}/>
+            <Chatcontainer selectedUser={selectedUser} setSelectedUser={setSelcetedUser}/>
+            <RightSidebar selectedUser={selectedUser} setSelectedUser={setSelcetedUser}/>
         </div>
     </div>
   )
