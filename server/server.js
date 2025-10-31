88fd0baa8a62a6172e00795d46aead94e5626e67
SwayamWakodikar <swayam.w06@gmail.com>
2025-10-31 22:28:40 +0530

    added usermodel

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import axios from 'axios';
 import cors from 'cors';
-
+import User from './src/models/userModel'
 app.use(cors);
 
 app.get('/',(req,res)=>{
---
+++ server/src/config/db.js
@@ -0,0 +1,4 @@
+import User from "../models/userModel";
+import mongoose from "mongoose";
+
+mongoose.connect
