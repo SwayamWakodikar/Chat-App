d24a21450286cdcce78d74ba629e7976bc5f4230
SwayamWakodikar <swayam.w06@gmail.com>
2025-10-31 23:12:54 +0530

    connected mongodb

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
 import express from 'express';
+import 'dotenv/config';
 // import axios from 'axios';
 import cors from 'cors';
-// import User from './src/models/userModel.js'
+import User from './src/models/userModel.js'
 import connectDB from './src/config/db.js';
 
 connectDB()
 const port=3000;
 const app=express();
-app.use(cors);
+// app.use(cors);
 
 app.get('/',(req,res)=>{
     console.log("Backend is running ");
--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
-import appconfig from "../../dotenv.js";
-const MONGODB_URI=appconfig.MONGODB_URI
-console.log(MONGODB_URI);
-const connectDB=async ()=>{
+// import dotenv from 'dotenv';
+// dotenv.config()
+const MONGODB_URI=process.env.MONGO_URI;
+const connectDB=async ()=>{         
     mongoose.connection.on("connected",()=>{
         console.log("connected");
     });
