54cc354a2e32bec18ed64b82f628fd29eaa373bc
SwayamWakodikar <swayam.w06@gmail.com>
2025-11-03 00:33:49 +0530

    --

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -9,7 +9,7 @@ const Homepage = () => {
   return (
     <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'>
         <div className='backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%] grid grid-cols-3 relative'>
-            <Sidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser}/>
+            {/* <Sidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser}/> */}
             <Chatcontainer selectedUser={selectedUser} setSelectedUser={setSelectedUser}/>
             <RightSidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser}/>
         </div>
