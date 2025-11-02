88ed676ea0186a7407ceeda880f9e8b795f32638
SwayamWakodikar <swayam.w06@gmail.com>
2025-11-02 20:33:15 +0530

    Tested Socket.js (Message not sent(BUG))

---
+++ server/src/sockets/socketTest.js
@@ -0,0 +1,26 @@
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:9000", {
+  transports: ["websocket"], 
+});
+
+socket.on("connect", () => {
+  console.log("✅ Connected to socket server:", socket.id);
+
+  socket.emit("join", "user123");
+
+  socket.emit("send_message", {
+    sender: "user123",
+    receiver: "user456",
+    content: "Hello world",
+    timestamps: new Date(),
+  });
+});
+
+socket.on("newMessage", (msg) => {
+  console.log("💬 New message received:", msg);
+});
+
+socket.on("disconnect", () => {
+  console.log("❌ Disconnected");
+});
