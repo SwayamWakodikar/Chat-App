6d7085287da435921596c34e60a245be1d190bb2
SwayamWakodikar <swayam.w06@gmail.com>
2025-11-14 21:57:15 +0530

    started socket on client side

--- client/src/components/Chatcontainer.jsx
+++ client/src/components/Chatcontainer.jsx
@@ -1,5 +1,23 @@
 import React from "react";
+import {Server, Socket} from "socket.io";
+import {io} from "socket.io-client";
 const Chatcontainer = () => {
+const socket = io("http://localhost:9000", {
+  transports: ["websocket"], 
+});
+
+socket.on("connect", () => {
+  console.log("Connected to socket server:", socket.id);
+
+  socket.emit("join", "user123");
+
+  socket.emit("send_message", {
+    sender: "user123",
+    receiver: "user456",
+    content: "Hello world",
+    timestamps: new Date(),
+  });
+});
   return (
     <div className="flex ">
       Chat-Container
